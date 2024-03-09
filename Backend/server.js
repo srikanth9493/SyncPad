d03fccc20420eb8801121ec572766577e2d2670b
Srikanth tekumudi <srikanth.tekumudi@phenompeople.com>
2024-03-09 16:26:43 +0530

    removed redundent code

--- Backend/server.js
+++ Backend/server.js
@@ -8,10 +8,9 @@ const {Server}=require('socket.io');
 const server=http.createServer(app);
 const io=new Server(server);
 
-app.use(express.static('build'));
 
-app.use((req,res,next) => {
-    res.sendFile(path.join(__dirname,"build","index.html"));
+app.use("/",(req,res)=>{
+    res.json({message:"hello wokrikng"})
 })
 
 const userSocketMap = {};
